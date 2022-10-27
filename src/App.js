830bdb457e0bacc47c87b2a753142f78b52f1e1b
Mary Buckley <mary.buckley@concerthealth.io>
2022-10-27 12:00:55 +0100

    built React app

--- src/App.js
+++ src/App.js
@@ -1,22 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+import { InputText } from "primereact/inputtext";
+import { Button } from "primereact/button";
+import { Toast } from "primereact";
+
+import "primereact/resources/themes/lara-light-indigo/theme.css"; //theme
+import "primereact/resources/primereact.min.css"; //core css
+import "primeicons/primeicons.css"; //icons
+import { useRef, useState } from "react";
 
 function App() {
+  const { text, setText } = useState("");
+  const toastRef = useRef();
+  const onButtonClick = () => {
+    if (text)
+      toastRef.current.show({
+        severity: "info",
+        summary: "success",
+        detail: "text",
+      });
+    else
+      toastRef.current.show({
+        severity: "error",
+        summary: "Error",
+        detail: "Value is required",
+      });
+  };
+
   return (
     <div className="App">
+      <Toast ref={toastRef} />
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <span className="p-float-label">
+          <InputText
+            id="input-txt"
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+          />
+          <label htmlFor="input-txt">Name</label>
+        </span>
+        <br />
+        <Button
+          type="button"
+          label="Submit"
+          icon="pi pi-check"
+          onClick={onButtonClick}
+        ></Button>
       </header>
     </div>
   );
